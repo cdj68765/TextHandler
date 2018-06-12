94cd96ca5b736fc146151e58ce62f4bbf8315e9c
cdj68765 <cdj68765@163.com>
2018-06-13 01:16:38 +0800

    添加项目文件。

---
+++ TextHandler/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDataReader;
+
+namespace TextHandler
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var run = new Run();
+            Console.ReadLine();
+        }
+
+    class Run
+        {
+            StringCompute String = new StringCompute();
+            Dictionary<string, string> NameList= new Dictionary<string,string>();
+            internal Run()
+            {
+                string path = "13w_梦.txt";
+                foreach (var VARIABLE in File.ReadAllLines("名字表.txt"))
+                {
+                    var TN = VARIABLE.Split('|');
+                    NameList.Add(TN[0], TN[1]);
+                }
+                var Txt = new List<string>(File.ReadAllLines(@"13w_梦.txt"));
+                SaveToFile(path, TextHandler(Txt));
+            }
+
+
+            private string[] TextHandler(List<string> OriText, int Chapter = 13)
+            {
+                var Exl = 读取并分析分类表();
+                var SaveText = new List<string>();
+                var TempText = new List<string>();
+                bool StartRec = false;
+                string Name = "";
+                string Text = "";
+                var Command = new HashSet<string>();
+                foreach (var Oridat in OriText)
+                {
+                    if (Oridat.StartsWith("&"))
+                    {
+                        Command.Add(Oridat);
+                    }
+
+                    if (StartRec)
+                    {
+                        if (Oridat[Oridat.Length - 1] != '”')
+                        {
+                            Text += Oridat + "\n";
+                            continue;
+                        }
+
+                        Text += Oridat;
+                        var Ein = Text.Replace("“", "").Replace("”", "");
+                        Text = "";
+                        string Zwei = null;
+                        if (Ein.IndexOf(@"{{不停}}", StringComparison.Ordinal) != -1)
+                        {
+                            Ein = Ein.Replace(@"{{不停}}", "");
+                            Zwei = Exl[Name].FirstOrDefault(x => x.Value.StartsWith(Ein)).Key;
+                            CheckTextExists();
+                        }
+                        else if (Ein.IndexOf(@"{{跳过}}", StringComparison.Ordinal) == -1)
+                        {
+                            Zwei = Exl[Name].FirstOrDefault(x =>
+                            {
+                                String.Compute(Ein, x.Value);
+                                return (float) String.ComputeResult.Rate > 0.8;
+                            }).Key;
+                            CheckTextExists();
+                            //   Zwei = Exl[Name].FirstOrDefault(x => x.Value == Ein).Key;
+                        }
+
+                        void CheckTextExists()
+                        {
+                            if (!string.IsNullOrWhiteSpace(Zwei))
+                            {
+                                var AddS = $"&{NameList[Name]}={Chapter}_{Zwei}.ogg";
+                                if (!Command.Contains(AddS))
+                                {
+                                    SaveText.Add(AddS);
+                                }
+
+                            }
+                            else
+                            {
+                                Trace.WriteLine($"未找到文本，行数:{SaveText.Count}");
+                            }
+                        }
+                        StartRec = false;
+                    }
+
+                    if (Oridat.StartsWith("【"))
+                    {
+                        Name = Oridat.Replace("【", "").Replace("】", "");
+                        if (!string.IsNullOrWhiteSpace(Name))
+                        {
+                            if (Exl.ContainsKey(Name))
+                            {
+                                SaveText.AddRange(TempText.ToArray());
+                                TempText = new List<string>();
+                                StartRec = true;
+                            }
+                        }
+                    }
+
+                    TempText.Add(Oridat);
+                }
+
+                SaveText.AddRange(TempText.ToArray());
+                return SaveText.ToArray();
+            }
+
+            private void SaveToFile(string path, string[] Date)
+            {
+                var Fol = new DirectoryInfo("Save");
+                if (!Directory.Exists("Save"))
+                {
+                    Fol.Create();
+                }
+
+                for (int i = 0; i < Date.Length; i++)
+                {
+                    Date[i] =i+":"+ Date[i];
+                }
+                File.WriteAllLines($"{Fol.FullName}\\{path}", Date);
+            }
+            Dictionary<string, Dictionary<string, string>> 读取并分析分类表(string Path = "13.xlsx")
+            {
+                var 分类列表 = new Dictionary<string, Dictionary<string, string>>();
+                using (var reader =
+                    ExcelReaderFactory.CreateReader(new FileStream(Path, FileMode.Open, FileAccess.Read,
+                        FileShare.ReadWrite)))
+                {
+                    foreach (DataTable Table in reader.AsDataSet().Tables)
+                    {
+                        var Dat = new Dictionary<string, string>();
+                        foreach (DataRow Date in Table.Rows)
+                        {
+                            Dat.Add(Date[0].ToString(), Date[2].ToString().Replace("“","").Replace("”",""));
+                        }
+
+                        分类列表.Add(Table.TableName, Dat);
+                    }
+                }
+                return 分类列表;
+            }
+        }
+    }
+}
